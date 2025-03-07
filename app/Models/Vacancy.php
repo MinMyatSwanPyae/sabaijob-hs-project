67e5f7933dc746d50a6bccd7d4b706069acc609d
MinMyatSwanPyae <73636287+MinMyatSwanPyae@users.noreply.github.com>
2025-03-07 16:54:44 +0700

    adding the relations into Vacancy Model :computer:

--- app/Models/Vacancy.php
+++ app/Models/Vacancy.php
@@ -10,4 +10,16 @@ class Vacancy extends Model
 {
     use HasFactory;
     //
+
+    // Model Relations// 
+    public function company()
+    {
+        return $this->belongsTo(company::class);
+    }
+
+    public function application()
+    {
+        return $this->belongsToMany(company::class); 
+    }
 }
+
