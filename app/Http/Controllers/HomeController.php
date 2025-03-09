8d64995e7e58b4b90756ae9d6223e529dd3e498e
MinMyatSwanPyae <73636287+MinMyatSwanPyae@users.noreply.github.com>
2025-03-09 11:04:44 +0700

    REFACTOR 🔨 Switch to the new standard: welcome -> home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class WelcomeController extends Controller{
+class HomeController extends Controller
+{
 
     public function index(){
 
