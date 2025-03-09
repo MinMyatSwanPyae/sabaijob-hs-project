fee4bdc0052b4d44c691ef6d57878c99ebec1b8e
MinMyatSwanPyae <73636287+MinMyatSwanPyae@users.noreply.github.com>
2025-03-10 00:48:46 +0700

    creating header and footer components :star:

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-pink-500 text-black">
+    <div class="max-w-7xl h-10">
+        <div>Our great footer</div>
+    </div>
+</div>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,13 @@
+<body>
+    <div class="bg-pink-500 text-black">
+        <div class="max-w-7xl m-auto flex justify-between items-center h-20">
+            <div class="font-bold text-lg">Sabai Job</div>
+            <ul class="flex gap-x-6">
+                <li><a href="/vacancies">Vacancies</a></li>
+                <li><a href="/companies"> Companies </a></li>
+                <li>My Application</li>
+                <li>Contact us</li>
+            </ul>
+            <div>Login</div>
+        </div>
+    </div>
